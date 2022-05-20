123d8e5da199256e5eaa7aa53862f7391862336e
Ringo Hoffmann <contact@zekro.de>
2022-05-20 20:57:42 +0200

    add upload status pages

--- web/src/routes/Upload.tsx
+++ web/src/routes/Upload.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router';
 import styled from 'styled-components';
 import { CreateSoundRequest, Sound } from '../api';
@@ -9,11 +9,27 @@ import { SoundEditor } from '../components/SoundEditor';
 import { useApi } from '../hooks/useApi';
 import { useSnackBar } from '../hooks/useSnackBar';
 
+import Loading0 from '../../assets/loading/0.gif';
+import Loading1 from '../../assets/loading/1.gif';
+import Loading2 from '../../assets/loading/2.gif';
+import Loading3 from '../../assets/loading/3.gif';
+import Loading4 from '../../assets/loading/4.gif';
+import Loading5 from '../../assets/loading/5.gif';
+import { randomFrom } from '../util/rand';
+
+const LOADINGS = [Loading0, Loading1, Loading2, Loading3, Loading4, Loading5];
+
 type Props = {};
 
 const StyledFileDrop = styled(FileDrop)`
   margin-bottom: 1.5em;
   width: 100%;
+`;
+
+const LoadingContainer = styled.div`
+  > img {
+    height: 14em;
+  }
 `;
 
 export const UploadRoute: React.FC<Props> = ({}) => {
@@ -22,13 +38,17 @@ export const UploadRoute: React.FC<Props> = ({}) => {
   const fetch = useApi();
   const nav = useNavigate();
   const { show } = useSnackBar();
+  const [state, setState] = useState(0);
+  const randomImageRef = useRef<string>(randomFrom(LOADINGS));
 
   const _create = async () => {
     if (!file || !sound) return;
     try {
       const req = { ...sound };
+      setState(1);
       const res = await fetch((c) => c.soundsUpload(file));
       req.upload_id = res.upload_id;
+      setState(2);
       await fetch((c) => c.soundsCreate(req));
       nav(-1);
       show(
@@ -37,7 +57,10 @@ export const UploadRoute: React.FC<Props> = ({}) => {
         </span>,
         'success',
       );
-    } catch {}
+    } catch {
+    } finally {
+      setState(0);
+    }
   };
 
   useEffect(() => {
@@ -47,15 +70,27 @@ export const UploadRoute: React.FC<Props> = ({}) => {
   return (
     <RouteContainer maxWidth="50em">
       <h1>Upload</h1>
-      <StyledFileDrop file={file} onFileInput={setFile} />
-      <SoundEditor
-        isNew
-        disabled={!file}
-        sound={sound}
-        updateSound={setSound}
-        onCancel={() => nav(-1)}
-        onSave={_create}
-      />
+      {(state === 0 && (
+        <>
+          <StyledFileDrop file={file} onFileInput={setFile} />
+          <SoundEditor
+            isNew
+            disabled={!file}
+            sound={sound}
+            updateSound={setSound}
+            onCancel={() => nav(-1)}
+            onSave={_create}
+          />
+        </>
+      )) || (
+        <LoadingContainer>
+          <img src={randomImageRef.current} />
+          <p>
+            {state === 1 && 'Uploading sound ...'}
+            {state === 2 && 'Processing sound ...'}
+          </p>
+        </LoadingContainer>
+      )}
     </RouteContainer>
   );
 };
