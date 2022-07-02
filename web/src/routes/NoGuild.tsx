8203ce90488096ae805e9e311ee9557a15156ed5
zekro <private@zekro.de>
2022-07-02 17:05:05 +0000

    add logout button to no guilds page

--- web/src/routes/NoGuild.tsx
+++ web/src/routes/NoGuild.tsx
@@ -1,12 +1,28 @@
+import styled from 'styled-components';
+import { Button } from '../components/Button';
 import { RouteContainer } from '../components/RouteContainer';
+import { ApiClientInstance } from '../instances';
+import { ReactComponent as IconLogout } from '../assets/logout.svg';
 
 type Props = {};
 
+const StyledButton = styled(Button)`
+  margin-top: 2em;
+`;
+
 export const NoGuildRoute: React.FC<Props> = ({}) => {
+  const _onLogout = () => {
+    window.location.assign(ApiClientInstance.logoutUrl());
+  };
+
   return (
     <RouteContainer center>
       <h3>Oh no. 😲</h3>
       <span>Looks like you are not sharing any guild with Yuri.</span>
+      <StyledButton variant="red" onClick={_onLogout}>
+        <IconLogout />
+        Logout
+      </StyledButton>
     </RouteContainer>
   );
 };
